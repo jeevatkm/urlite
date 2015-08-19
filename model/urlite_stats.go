793a08f2859d0e807da86f7031eae05f4f36ad1c
Jeevanandam M <jeeva@myjeeva.com>
2015-08-19 15:04:25 -0700

    urlite stats initial collection definition done

--- model/urlite_stats.go
+++ model/urlite_stats.go
@@ -1,1 +1,45 @@
 package model
+
+import (
+	"time"
+)
+
+type UrliteStats struct {
+	ID          string    `bson:"_id,omitempty" json:"id"`
+	Title       string    `bson:"title" json:"title"`
+	Note        string    `bson:"note" json:"note"`
+	Tags        []string  `bson:"tags" json:"tags"`
+	Clicks      int       `bson:"clicks" json:"clicks"`
+	CreatedBy   string    `bson:"cb" json:"-"`
+	CreatedTime time.Time `bson:"ct" json:"-"`
+	UpdatedBy   string    `bson:"ub" json:"-"`
+	UpdatedTime time.Time `bson:"ut" json:"-"`
+}
+
+type UrliteCountryStats struct {
+	ID             string    `bson:"_id,omitempty" json:"-"`
+	UrliteId       string    `bson:"urlite_id"`
+	CountryCodeISO string    `bson:"cc_iso" json:"country_code_iso"`
+	CountryCodeUN  string    `bson:"cc_un" json:"country_code_un"`
+	Clicks         string    `bson:"clicks" json:"clicks"`
+	ClickTime      time.Time `bson:"click_time" json:"click_time"`
+}
+
+type UrliteReferrerStats struct {
+	ID        string    `bson:"_id,omitempty" json:"-"`
+	UrliteId  string    `bson:"urlite_id"`
+	URL       string    `bson:"url" json:"url"`
+	Host      string    `bson:"host" json:"host"`
+	Clicks    string    `bson:"clicks" json:"clicks"`
+	ClickTime time.Time `bson:"click_time" json:"click_time"`
+}
+
+type UrliteLog struct {
+	ID             string    `bson:"_id,omitempty"`
+	UrliteId       string    `bson:"urlite_id"`
+	Domain         string    `bson:"domain"`
+	Refferrer      string    `bson:"referrer"`
+	CountryCodeISO string    `bson:"country_code_iso"`
+	CountryCodeUN  string    `bson:"country_code_un"`
+	CreatedTime    time.Time `bson:"ct"`
+}
